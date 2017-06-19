898852f99d695499e9d93368a3a6981dd999339a
rchaser53 <t.yoshizawa@sorabito.com>
2017-06-19 16:34:28 +0900

    3列の自動ソートのサンプルを作ろうとしたが失敗
    event.targetが取得できれば良いのだが、dataで設定したオブジェクトしか取得できなかった

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fluid container">
-    <div class="form-group form-group-lg panel panel-default">
+    <!--<div class="form-group form-group-lg panel panel-default">
       <div class="panel-heading">
         <h3 class="panel-title">Sortbale control</h3>
       </div>
@@ -10,9 +10,9 @@
         </div>
         <button type="button" class="btn btn-default" @click="orderList">Sort by original order</button>
       </div>
-    </div>
+    </div>-->
 
-    <div  class="col-md-3">
+    <!--<div  class="col-md-3">
         <draggable class="list-group" element="ul" v-model="list" :options="dragOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false"> 
           <transition-group type="transition" :name="'flip-list'">
             <li class="list-group-item" v-for="element in list" :key="element.order"> 
@@ -22,33 +22,53 @@
             </li> 
           </transition-group>
       </draggable>
+    </div>-->
+
+    <div class="col-md-3 col-md-3-height">
+      <draggable class="list-group" element="ul" v-model="list1" :options="dragOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false"> 
+          <transition-group type="transition" :name="'flip-list'">
+            <a class="list-group-item nyan" v-for="element in list1" :key="element.order"> 
+              {{element.name}}
+              <span class="badge">{{element.number}}</span>
+            </a> 
+          </transition-group>
+      </draggable>
     </div>
 
-     <div  class="col-md-3">
+     <div class="col-md-3 col-md-3-height">
       <draggable element="span" v-model="list2" :options="dragOptions" :move="onMove"> 
           <transition-group name="no" class="list-group" tag="ul">
-            <li class="list-group-item" v-for="element in list2" :key="element.order"> 
-              <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'" @click=" element.fixed=! element.fixed" aria-hidden="true"></i>
+            <li class="list-group-item nyan" v-for="element in list2" :key="element.order"> 
               {{element.name}}
-              <span class="badge">{{element.order}}</span>
+              <span class="badge">{{element.number}}</span>
             </li> 
           </transition-group>
       </draggable>
     </div>
 
+     <div class="col-md-3 col-md-3-height">
+      <draggable element="span" v-model="list3" :options="dragOptions" :move="onMove"> 
+          <transition-group name="no" class="list-group" tag="ul">
+            <li class="list-group-item nyan" v-for="element in list3" :key="element.order"> 
+              {{element.name}}
+              <span class="badge">{{element.number}}</span>
+            </li> 
+          </transition-group>
+      </draggable>
+    </div>
 
-    <div  class="list-group col-md-3">
-      <pre>{{listString}}</pre>
-    </div>
-     <div  class="list-group col-md-3">
-      <pre>{{list2String}}</pre>
-    </div>
   </div>
 </template>
 
 <script>
 import draggable from 'vuedraggable'
-const message = [ 'vue.draggable', 'draggable', 'component', 'for', 'vue.js 2.0', 'based' , 'on', 'Sortablejs' ]
+// const message1 = [ 'vue.draggable', 'for', 'on']
+// const message2 = [ 'draggable', 'vue.js 2.0', 'Sortablejs' ]
+// const message3 = [ 'component', 'based']
+
+const message = [
+  'vue.draggable', 'draggable', 'component', 'for', 'vue.js 2.0', 'based', 'on', 'Sortablejs'
+]
 
 export default {
   name: 'hello',
@@ -57,22 +77,46 @@ export default {
   },
   data () {
     return {
-      list: message.map( (name,index) => {return {name, order: index+1, fixed: false}; }),
-      list2:[],
+      baseList: message.map((name, index) => {
+        return {name, order: index+1, fixed: false, number: index+1}
+      }),
+      list1: message.map((name, index) => {
+        return {name, order: index+1, fixed: false, number: index+1}
+      }).filter((elem) => elem.order % 3 === 1),
+      list2: message.map((name, index) => {
+        return {name, order: index+1, fixed: false, number: index+1}
+      }).filter((elem) => elem.order % 3 === 2),
+      list3: message.map((name, index) => {
+        return {name, order: index+1, fixed: false, number: index+1}
+      }).filter((elem) => elem.order % 3 === 0),
       editable:true,
       isDragging: false,
       delayedDragging:false
     }
   },
   methods:{
-    orderList () {
-      this.list = this.list.sort((one,two) =>{return one.order-two.order; })
-    },
     onMove ({relatedContext, draggedContext}) {
       const relatedElement = relatedContext.element;
       const draggedElement = draggedContext.element;
-      return (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
-    }
+      if (relatedElement && relatedElement.fixed) return false;
+
+      this.sortItem(draggedElement, relatedElement)
+      return true;
+    },
+    sortItem (draggedElement, relatedElement) {
+      // const dropedOrder = relatedElement.order;
+      // const draggedOrder =draggedElement.order;
+
+      // this.baseList[dropOrder-1] = dr
+
+      // 上半分にドラッグされたのか下半分にドラッグされたのかが判定できないので駄目そう
+      // eventtargetがそのまま欲しかった
+      console.log(draggedElement, relatedElement)
+    },
+
+    // orderList () {
+    //   this.list = this.list.sort((one,two) =>{return one.order-two.order; })
+    // },
   },
   computed: {
     dragOptions () {
@@ -83,12 +127,9 @@ export default {
         ghostClass: 'ghost'
       };
     },
-    listString(){
-      return JSON.stringify(this.list, null, 2);  
-    },
-    list2String(){
-      return JSON.stringify(this.list2, null, 2);  
-    }
+    // list1: this.baseList.filter((elem) => {
+    //   return elem % 3 === 1;
+    // }),
   },
   watch: {
     isDragging (newValue) {
@@ -97,7 +138,7 @@ export default {
         return
       }
       this.$nextTick( () =>{
-           this.delayedDragging =false
+        this.delayedDragging =false
       })
     }
   }
@@ -119,7 +160,7 @@ export default {
 }
 
 .list-group {
-  min-height: 20px;
+  min-height: 150px;
 }
 
 .list-group-item {
@@ -129,4 +170,12 @@ export default {
 .list-group-item i{
   cursor: pointer;
 }
+
+.nyan {
+  width: 250px;
+  height: 250px;
+
+}
+
+
 </style>
